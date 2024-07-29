d284198a6beb0e75072de96551728dceb6cde695
sansawasansan <ishiwatari0722@gmail.com>
2024-07-29 18:44:40 +0900

    create the R scripts for organizing the covariate data Covariationデータのクリーニングのためのスクリプト

---
+++ cleaning/03_covariate data.R
@@ -0,0 +1,10 @@
+###############################################################################=
+# 2024 RA bootcamp warm-up task-----
+# 01_cleaning
+# 03_Covariates Data----
+# BOSTWICK, Valerie, Stefanie Fischer, and Matthew Lang. (2022). 
+# "Semesters or Quarters? TheEffect of the Academic Calendar on Postsecondary Student Outcomes.” 
+# American EconomicJournal: Economic Polic
+# start:2024/07/29
+# end:
+###############################################################################=
