eed8e866f2bc9702e8f254b9b5dc578a14ae45d3
sansawasansan <ishiwatari0722@gmail.com>
2024-07-29 14:49:36 +0900

    change datalist name, add total and men grad rate dataframe_listからdlistにデータのリストの名前変更と合計・男の卒業率の列の追加、合計・男・女の卒業率を有効桁数3桁で表示した

--- cleaning/01_graduate data.R
+++ cleaning/01_graduate data.R
@@ -23,33 +23,48 @@ library(tidyverse)
 # graduate data: outcome(1991-2016).xlsx
 # data/warmup training package/01_data/raw/outcome
 
-dataframe_list <- list()
+d_list <- list()
 dataframe_names <- list.files(path = "./data/warmup training package/01_data/raw/outcome/", full.names = FALSE, pattern = "\\.xlsx$") %>%
   gsub(".xlsx$", "", .)
 
 for (name in dataframe_names) {
   filepath <- paste("./data/warmup training package/01_data/raw/outcome/", name, ".xlsx", sep="")
-  dataframe_list[[name]] <- readxl::read_excel(filepath)  
+  d_list[[name]] <- readxl::read_excel(filepath)  
 }
-dataframe_list # outcome List data by year
+d_list # outcome List data by year
 
 #------------------------------------------------------------------------------=
 #02 change the women_graduate_4yr rate----
 # 女子学生の4年卒業率に0.01をかけて、0から1のスケールに変更しなさい
 #------------------------------------------------------------------------------=
-for(i in seq_along(dataframe_list)){
-  dataframe_list[[i]]$women_gradrate_4yr <- dataframe_list[[i]]$women_gradrate_4yr * 0.01
+for(i in seq_along(d_list)){
+  d_list[[i]]$women_gradrate_4yr <- d_list[[i]]$women_gradrate_4yr * 0.01
 }
-dataframe_list
+d_list
 
 #------------------------------------------------------------------------------=
 #03 add tot_gradrate_4yr rate the men_gradrate_4yr rate and 
 # 男女合計の4年卒業率と男子学生の4年卒業率を計算し、新たな列として追加しなさい
 #------------------------------------------------------------------------------=
+for(i in seq_along(d_list)){
+  d_list[[i]]$totcohortsize <- as.numeric(d_list[[i]]$totcohortsize)
+  d_list[[i]]$tot_gradrate_4yr <- d_list[[i]]$tot4yrgrads/d_list[[i]]$totcohortsize 
+  d_list[[i]]$m_4yrgrads <- as.numeric(d_list[[i]]$m_4yrgrads)
+  d_list[[i]]$men_gradrate_4yr <- d_list[[i]]$m_4yrgrads/d_list[[i]]$m_cohortsize 
+  }
+d_list[[1]]
+#------------------------------------------------------------------------------=
+#04 change the women_graduate_4yr rate----
+#計算した卒業率を有効数字3桁に調整しなさい
+#------------------------------------------------------------------------------=
+for(i in seq_along(d_list)){
+  d_list[[i]]$tot_gradrate_4yr <- round(d_list[[i]]$tot_gradrate_4yr,3)
+  d_list[[i]]$men_gradrate_4yr <- round(d_list[[i]]$men_gradrate_4yr,3)
+  d_list[[i]]$women_gradrate_4yr <- round(d_list[[i]]$women_gradrate_4yr,3)
+}
+d_list[[1]]
 
-
-
-
-
-
+#------------------------------------------------------------------------------=
+#05 change time period to 1991-2010-----
+# 1991年から2010年までのデータフレームに変形しなさい
 #------------------------------------------------------------------------------=
