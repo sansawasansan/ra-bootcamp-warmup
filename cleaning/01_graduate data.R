65ed1dec16bd3a2c061b2ed6e9c35a6f605ecd87
sansawasansan <ishiwatari0722@gmail.com>
2024-07-29 10:57:13 +0900

    script for cleaning the graduate dataset graduate datasetを整えるためのスクリプト

---
+++ cleaning/01_graduate data.R
@@ -0,0 +1,16 @@
+###############################################################################=
+# 2024 RA bootcamp warm-up task-----
+# 01_cleaning
+# 01_graduate data ----
+# BOSTWICK, Valerie, Stefanie Fischer, and Matthew Lang. (2022). 
+# "Semesters or Quarters? TheEffect of the Academic Calendar on Postsecondary Student Outcomes.” 
+# American EconomicJournal: Economic Polic
+# start:2024/07/29
+# end:
+###############################################################################=
+#------------------------------------------------------------------------------=
+# 01_graduate data ----
+#------------------------------------------------------------------------------=
+# preparation ----
+rm(list=ls())
+
